f3a38e0e7377549e7c530c82d46f95e66638561f
richardhendricksen <richard.hendricksen@gmail.com>
2019-05-07 20:05:53 +0200

    just chrome for now

--- src/test/java/nl/codecontrol/cucumber/driver/Setup.java
+++ src/test/java/nl/codecontrol/cucumber/driver/Setup.java
@@ -1,13 +1,10 @@
 package nl.codecontrol.cucumber.driver;
 
 import cucumber.api.java.Before;
-import cucumber.api.java.BeforeStep;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Setup {
 
@@ -24,22 +21,8 @@ public class Setup {
 
     @Before
     public void setWebDriver() {
-        String browser = System.getProperty("browser");
-        if (browser == null) {
-            browser = "chrome";
-        }
-        switch (browser) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("start-maximized");
-                driver = new ChromeDriver(chromeOptions);
-                break;
-            case "firefox":
-                driver = new FirefoxDriver();
-                driver.manage().window().maximize();
-                break;
-            default:
-                throw new IllegalArgumentException("Browser \"" + browser + "\" isn't supported.");
-        }
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
     }
 }
